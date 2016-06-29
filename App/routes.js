933fca1e9a8327f77a5956b8cd72afc487fa8b09
danieloprado <danieloprado@outlook.com>
2016-06-29 13:20:31 -0300

    route js

--- App/routes.js
+++ App/routes.js
@@ -1,27 +1,25 @@
-(function(angular) {
+(function (angular) {
   'use strict';
 
   angular.module('icbApp')
-    .config([
-      '$routeProvider',
-      '$locationProvider',
-      function($routeProvider, $locationProvider) {
+    .config(['$routeProvider', '$locationProvider', configRoutes]);
 
-        $locationProvider.html5Mode({
-          enabled: true,
-          requireBase: false
-        });
+  function configRoutes($routeProvider, $locationProvider) {
 
-        $routeProvider
-          .when('/', {
-            templateUrl: '/views/pages/dashboard/home/home.html',
-            controller: 'icbApp.homeCtrl'
-          })
-          .otherwise({
-            redirectTo: "/"
-          });
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+    });
 
-      }
-    ]);
+    $routeProvider
+      .when('/', {
+        templateUrl: '/views/pages/dashboard/home/home.html',
+        controller: 'icbApp.homeCtrl'
+      })
+      .otherwise({
+        redirectTo: "/"
+      });
+
+  }
 
 })(angular);
